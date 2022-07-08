9f42a202f8385eb2b8c4552a8e502f01b13549ac
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-08 22:59:46 +0500

    Edit utils.js FIle

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -3,6 +3,7 @@
 import Meal from './meal.js';
 import Likes from './likes.js';
 import Comment from './comment.js';
+import Reservation from './reservation.js';
 
 const mainDiv = document.querySelector('#food-items');
 const mealDetailsContent = document.querySelector('.meal-details-content');
@@ -13,39 +14,52 @@ const mealList = async (data) => {
   const mealCount = document.querySelector('#meal-count');
   const totalMeals = Meal.mealsCounter(data);
   mealCount.innerHTML = totalMeals;
+
   for (let i = 0; i < totalMeals; i += 1) {
     const foodDiv = document.createElement('div');
     foodDiv.classList.add('col-lg-4');
+
     const listItem = document.createElement('div');
     listItem.id = data[i].idMeal;
     foodDiv.setAttribute('data-id', `${data[i].idMeal}`);
     listItem.classList.add('meal-item', 'text-center', 'card', 'shadow', 'mb-4', 'border-0');
+
     const itemImage = document.createElement('div');
     itemImage.classList.add('meal-img');
+
     const image = document.createElement('img');
     image.src = data[i].strMealThumb;
+
     itemImage.appendChild(image);
     listItem.appendChild(itemImage);
+
     const itemText = document.createElement('div');
     itemText.classList.add('meal-name', 'd-flex', 'pt-3', 'justify-content-center', 'align-items-center');
+
     const mealName = document.createElement('h3');
     mealName.classList.add('fs-6', 'me-2', 'pt-1');
     mealName.innerHTML = data[i].strMeal;
+
     const likeBtn = document.createElement('i');
     likeBtn.classList.add('bi', 'bi-heart', 'like-btn', 'text-danger');
     likeBtn.id = data[i].idMeal;
+
     itemText.appendChild(mealName);
     itemText.appendChild(likeBtn);
     listItem.appendChild(itemText);
+
     const mealLikes = document.createElement('div');
     const numOfLikes = document.createElement('small');
     numOfLikes.innerHTML = '0 likes';
     mealLikes.appendChild(numOfLikes);
     listItem.appendChild(mealLikes);
+
     const commentContainer = document.createElement('div');
     commentContainer.classList.add('comment', 'mb-4', 'mt-2');
+
     const reservationContainer = document.createElement('div');
     reservationContainer.classList.add('comment', 'mb-4', 'mt-2');
+
     const button = document.createElement('button');
     button.classList.add('btn', 'btn-warning', 'rounded-pill', 'px-4', 'shadow', 'comment-btn');
     button.setAttribute('id', `${data[i].idMeal}`);
@@ -53,6 +67,7 @@ const mealList = async (data) => {
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#staticBackdrop');
     button.innerHTML = 'comments';
+
     const reservationBtn = document.createElement('button');
     reservationBtn.classList.add('btn', 'btn-warning', 'rounded-pill', 'px-4', 'shadow', 'reservation-btn');
     reservationBtn.setAttribute('id', `${data[i].idMeal}`);
@@ -60,14 +75,18 @@ const mealList = async (data) => {
     reservationBtn.setAttribute('data-bs-toggle', 'modal');
     reservationBtn.setAttribute('data-bs-target', '#staticBackdrop');
     reservationBtn.innerHTML = 'reservations';
+
     commentContainer.appendChild(button);
     reservationContainer.appendChild(reservationBtn);
     listItem.appendChild(commentContainer);
     listItem.appendChild(reservationContainer);
+
     foodDiv.appendChild(listItem);
     mainDiv.appendChild(foodDiv);
+
     const likesArray = await Likes.totalLikes();
     Likes.likesCount(likeBtn, likesArray, numOfLikes);
+
     likeBtn.addEventListener('click', async (e) => {
       await Likes.likeGenerator(likeBtn.id);
       const newLikes = await Likes.totalLikes();
@@ -121,6 +140,55 @@ const mealModal = async (meal) => {
   Comment.getComment(meal);
 };
 
+const mealReservationModal = async (meal) => {
+  [meal] = meal;
+  mealDetailsContent.innerHTML = `
+    <h2 class = "recipe-title">${meal.strMeal}</h2>
+    <p class = "recipe-category">${meal.strCategory}</p>
+    <div class = "recipe-instruct">
+      <h3>Instructions:</h3>
+      <p>${meal.strInstructions}</p>
+    </div>
+    <div class = "recipe-meal-img">
+      <img src = "${meal.strMealThumb}" alt = "">
+    </div>
+    <h3 class="m-3 comment-count"></h3>
+    <div class="d-flex justify-content-center align-items-center">
+      <ul id="list-comment" class="list-unstyled">
+      </ul>
+    </div>
+    <h3 class="m-3">Add Reservation</h3>
+    <form autocomplete="off" class="w-50 mx-auto">
+      <input type="text" class="form-control w-75 mx-auto mb-2" id="name" placeholder="Your name">
+      <input type="date" class="form-control w-75 mx-auto mb-2" id="startDate" placeholder="Start Date">
+      <input type="date" class="form-control w-75 mx-auto mb-2" id="endDate" placeholder="End Date">
+      <button type="button" class="btn btn-secondary btn-warning reservationBtn">Reservation</button>
+    </form>
+  `;
+  mealDetailsContent.parentElement.classList.add('showComment');
+
+  const reservationBtn = document.querySelector('.reservationBtn');
+  reservationBtn.addEventListener('click', () => {
+    const username = document.querySelector('#name').value;
+    const dateStart = document.querySelector('#startDate').value;
+    const dateEnd = document.querySelector('#endDate').value;
+    const newData = {
+      item_id: meal.idMeal,
+      username,
+      date_start: dateStart,
+      date_end: dateEnd,
+    };
+    Reservation.postReservation(newData);
+    document.querySelector('#name').value = '';
+    document.querySelector('#startDate').value = '';
+    document.querySelector('#endDate').value = '';
+    setTimeout(() => {
+      Reservation.getReservation(meal);
+    }, 1000);
+  });
+  Reservation.getReservation(meal);
+};
+
 const getMeal = async (e) => {
   e.preventDefault();
   if (e.target.classList.contains('comment-btn')) {
@@ -128,6 +196,12 @@ const getMeal = async (e) => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`;
     const response = await fetch(url).then((response) => response.json()).then((data) => data);
     mealModal(response.meals);
+  }
+  if (e.target.classList.contains('reservation-btn')) {
+    const mealItem = e.target.parentElement.parentElement.parentElement;
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`;
+    const response = await fetch(url).then((response) => response.json()).then((data) => data);
+    mealReservationModal(response.meals);
   }
 };
 
