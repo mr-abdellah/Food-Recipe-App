6d5780626c87f67932c1acd6a43a31f66070e981
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-08 23:37:26 +0500

    edit reservation.test.js file for linter errors

--- src/__test__/reservation.test.js
+++ src/__test__/reservation.test.js
@@ -1,31 +1,30 @@
-import Reservation from '../reservation';
+import Reservation from '../modules/reservation.js';
 
 // test reservation counter function
 describe('test reservation counter function', () => {
-    it('test number of reservations from the API', () => {
-        const reservation = [
-            {
-                "username": "John",
-                "date_start": "2020-12-17",
-                "date_end": "2020-12-18"
-            },
+  it('test number of reservations from the API', () => {
+    const reservation = [
+      {
+        username: 'John',
+        date_start: '2020-12-17',
+        date_end: '2020-12-18',
+      },
 
-            {
-                "username": "Jane",
-                "date_start": "2021-1-12",
-                "date_end": "2021-1-17"
-            }
-        ];
+      {
+        username: 'Jane',
+        date_start: '2021-1-12',
+        date_end: '2021-1-17',
+      },
+    ];
 
-        expect(Reservation.totalReservation(reservation)).toBe(2);
-    });
+    expect(Reservation.totalReservation(reservation)).toBe(2);
+  });
 
-    it('test number of reservations from the API', () => {
-        const reservation = [
+  it('test number of reservations from the API', () => {
+    const reservation = [
 
-        ];
+    ];
 
-        expect(Reservation.totalReservation(reservation)).toBe(0);
-    });
-
+    expect(Reservation.totalReservation(reservation)).toBe(0);
+  });
 });
